b5a367359fa038c8bdc5f18c844d676eb3a1a743
we know <csh_do@naver.com>
2020-05-18 15:52:06 +0900

    파일명 변경 및 수정

--- web/PetitionVisualizer/src/main/java/team/cocopalm/PetitionVisualizer/controller/NewPetitionController.java
+++ web/PetitionVisualizer/src/main/java/team/cocopalm/PetitionVisualizer/controller/NewPetitionController.java
@@ -1,5 +1,22 @@
 package team.cocopalm.PetitionVisualizer.controller;
 
-public class PetitionVisualizerController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+
+// 만료 이전 청원 컨트롤러
+@Slf4j // log 객체 자동 생성
+@Controller
+public class NewPetitionController {
+	
+	@RequestMapping("/")
+	public String Home() {
+		// @Slfj4로 생성된 log 객체 이용해서 log 남김
+		log.info("log testing !");
+		
+		// newPetition.jsp 반환
+		return "newPetition";
+	}
 }
---
+++ web/PetitionVisualizer/src/main/webapp/WEB-INF/views/newPetition.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>만료 이전 청원</title>
+</head>
+<body>
+ 
+만료 이전 청원
+ 
+</body>
+</html>
