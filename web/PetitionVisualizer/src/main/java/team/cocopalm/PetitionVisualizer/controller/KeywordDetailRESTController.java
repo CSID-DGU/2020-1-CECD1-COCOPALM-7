5ae635afc75036a54c580ca0b8b82e2c0a7a8f48
we know <csh_do@naver.com>
2020-08-21 23:28:20 +0900

    추가한 ...Service.java  활용하도록 함수 수정

--- web/PetitionVisualizer/src/main/java/team/cocopalm/PetitionVisualizer/controller/KeywordDetailRESTController.java
+++ web/PetitionVisualizer/src/main/java/team/cocopalm/PetitionVisualizer/controller/KeywordDetailRESTController.java
@@ -1,42 +1,48 @@
 package team.cocopalm.PetitionVisualizer.controller;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 import team.cocopalm.PetitionVisualizer.model.Post;
+import team.cocopalm.PetitionVisualizer.model.PostKeyword;
+import team.cocopalm.PetitionVisualizer.service.PostKeywordService;
+import team.cocopalm.PetitionVisualizer.service.PostService;
 
 @Slf4j
 @RestController
 public class KeywordDetailRESTController {
+	@Autowired PostService postService;
+	@Autowired PostKeywordService postKeywordService;
+	
+	// ex) http://.../keyword/bestPetitions?keyword=코로나
 	@GetMapping("/keyword/bestPetitions")
-	public ArrayList<Post> BestPetitions() {
-		log.info("[/keyword/bestPetitions] 매핑!");
-		ArrayList<Post> posts = new ArrayList<>();
-		Post post = Post.builder()
-				.post_id(590411)
-				.category_id(16)
-				.title("연대보증채권을 소각해주세요")
-				.start_date(LocalDateTime.parse("2020-07-06 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-				.end_date(LocalDateTime.parse("2020-08-05 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-				.agree_count(514)
-				.is_new(0)
-				.build();
-		posts.add(post);
-		post = Post.builder()
-				.post_id(590410)
-				.category_id(2)
-				.title("일용직 청년층들 피해 막아주세요.")
-				.start_date(LocalDateTime.parse("2020-07-06 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-				.end_date(LocalDateTime.parse("2020-08-05 00:00:00", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")))
-				.agree_count(291)
-				.is_new(0)
-				.build();
-		posts.add(post);
+	public List<Post> BestPetitions(@RequestParam String keyword) throws Exception {
+		log.info("[/keyword/bestPetitions] 매핑! >> keyword : " + keyword);
+		
+		List<Post> posts = new ArrayList<Post>();
+		
+		// 만료 이전 청원 중 가장 많은 동의 : is_new == true >>>>>>>>>>> 현재 false를 추후에 만료 이전 데이터가 생기면 true로 바꾸기
+		posts.add(postService.selectMostAgreePetitionByKeyword(keyword, false));
+		// 만료 된 청원 중 가장 많은 동의 : is_new == false
+		posts.add(postService.selectMostAgreePetitionByKeyword(keyword, false));
+		
+		System.out.println(posts.get(0).getTitle());
+		
 		return posts;
+	}
+	
+	// 테스트 용
+	@GetMapping("/test")
+	public List<PostKeyword> Test() throws Exception {
+		log.info("테스트");
+		
+		return postKeywordService.test();
 	}
 }
